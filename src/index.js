e71a055878a5bf372729977ea9aae47f3185f813
veranika <veronica.stanevich@gmail.com>
2020-03-23 22:10:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    else {
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        return array[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    array.sort(function (a, b) {
+        return a - b;
+    });
+    return array[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let i = 0;
+    let sum = 0;
+    while (i < array.length - 1) {
+        sum += array[i];
+    }
+    return sum / (array.length - 1);
 }
